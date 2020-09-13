726ffd2e47f8119ff6b78f1a1401a8dfd8d6d60e
SimaAlaverdyan <sima.alaverdyan.02@gmail.com>
2020-09-13 16:35:11 +0400

    ff

--- index.php
+++ index.php
@@ -13,43 +13,6 @@
 	<div class="formdiv">
 		<h1>Contact form</h1>
 		<h4>Please fill in your information and we'll be sending your order in no time</h4><br>
-		<!-- <form class="form-inline" action="index.php" method="post">
-			<div class="form-row">
-				<div class="form-group col-lg-12 col-md-6">
-					<label class="labels">Your Name </label>
-					<input type="text" class="form-control" name="name" placeholder="FirstName">
-					<input type="text" class="form-control" name="sname" placeholder="SecondName"><br><br>
-				</div>
-			</div>
-			<div class="form-group col-lg-12 col-md-12">
-				<label class="labels">Your Email</label>
-				<input type="email" class="form-control" name="mail" placeholder="example@gmail.com"><br><br>
-			</div>
-			<div class="form-group col-lg-12 col-md-12">
-				<label class="datelabel">Date</label>
-				<input class="form-control " type="date" name="data">
-				<label class="phonelabel">Phone</label>
-				<input type="text" class="form-control" name="phone" placeholder="### ### ###"><br><br>
-			</div>
-			<div class="form-group col-lg-6 col-md-12">
-				<label class="commentlabel">Comment</label>
-				<textarea name="comment" class="form-control" rows="5" cols="40"></textarea><br><br>
-			</div>
-			<div class="form-row col-lg-6 col-md-12">
-				<div class="col">
-					<label class="labels">Gender</label>
-				</div>
-				<div class="col">
-					<input type="radio" name="gender" value="male"> Male
-				</div>
-				<div class="col">
-					<input type="radio" name="gender" value="female"> Female <br><br>
-				</div>
-				<div class="col">
-					<input type="submit" name="submit" value="Submit" class="btn btn-success subbottom">
-				</div>
-			</div>
-		</form> -->
 		<form method="post" action="index.php">
 			<div class="form-row">
 				<div class="form-group col-md-6">
@@ -107,7 +70,6 @@
 	if (isset($_POST['submit'])) {
 		unset($_POST['submit']);
 		include('routes/table.php');
-		// var_dump($_POST);
 	}
 	?>
 </body>
